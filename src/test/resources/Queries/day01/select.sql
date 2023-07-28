ce5482d5a9696359bcc1cdca46219ca678db8f00
Safiye_Mertcan <safiyemertcan@hotmail.com>
2023-07-28 15:53:54 +0200

    added select.sql

---
+++ src/test/resources/Queries/day01/select.sql
@@ -0,0 +1,23 @@
+select * from EMPLOYEES;
+
+select * from DEPARTMENTS;
+
+--SQL language is not case sensitive
+
+select FIRST_NAME from EMPLOYEES;
+
+select FIRST_NAME, LAST_NAME from EMPLOYEES;
+
+select SALARY, EMAIL from EMPLOYEES;
+
+-- display all information from locations table
+
+select * from LOCATIONS;
+
+--display country name and region id from countries
+
+select COUNTRY_NAME, REGION_ID from COUNTRIES;
+
+--display job id and job title from jobs table
+
+select JOB_ID, JOB_TITLE from JOBS;
