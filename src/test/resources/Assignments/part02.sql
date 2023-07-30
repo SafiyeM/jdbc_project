65a24136bb4bf1fad24a29d07ca9258312c6d0c6
Safiye_Mertcan <safiyemertcan@hotmail.com>
2023-07-30 10:08:02 +0200

    added assignments part2

--- src/test/resources/Assignments/part02.sql
+++ src/test/resources/Assignments/part02.sql
@@ -4,10 +4,6 @@ select STREET_ADDRESS|| ' ' || ' ' ||POSTAL_CODE|| ' ' ||CITY||' ' || STATE_PROV
 from LOCATIONS;
 
 -- 2. Display all information of the employee who has the minimum salary in employees table
-select FIRST_NAME, LAST_NAME, SALARY
-from EMPLOYEES
-order by SALARY desc ;
-
 SELECT * FROM employees
 WHERE salary = (SELECT MIN(salary) FROM employees);
 
@@ -21,13 +17,67 @@ where SALARY in ( select min(SALARY) from EMPLOYEES where SALARY > (select min(S
 
 
 -- 5. List all the employees who are making above the average salary;
-
+select * from EMPLOYEES
+where SALARY > (select avg(SALARY) from EMPLOYEES)
+order by SALARY desc;
 
 -- 6. List all the employees who are making less than the average salary
+select * from EMPLOYEES
+where SALARY < (select avg(salary) from EMPLOYEES)
+order by  SALARY desc;
+
 -- 7. Display manager name of 'Neena'
+select FIRST_NAME from EMPLOYEES
+where FIRST_NAME = 'Neena';
+
 -- 8. Find the 3rd maximum salary from the employees table (do not include duplicates)
+select distinct SALARY from EMPLOYEES
+order by salary desc; --> this query finds all salary in desc order
+
+select SALARY from (select distinct SALARY from EMPLOYEES order by salary desc )
+where rownum < 4; --> this query finds first three maximum salary
+
+--solution
+select SALARY from EMPLOYEES
+where SALARY = (select min(salary) from (select distinct SALARY from EMPLOYEES order by salary desc )
+                where rownum < 4); --> this displays 3rd maximum salary
+
 -- 9. Find the 5th maximum salary from the employees table (do not include duplicates)
+select distinct SALARY from EMPLOYEES
+order by SALARY desc; --> all salary in desc order
+
+select SALARY from (select distinct SALARY from EMPLOYEES order by SALARY desc)
+where rownum < 6; --> first five highest salary
+
+-- solution
+select SALARY from EMPLOYEES
+where SALARY = (select min(SALARY) from (select distinct SALARY from EMPLOYEES order by SALARY desc)
+                where rownum < 6); --> the 5th max salary
+
+
 -- 10. Find the 7th maximum salary from the employees table (do not include duplicates)
+select SALARY from EMPLOYEES
+where salary = (select min(SALARY) from (select distinct SALARY from EMPLOYEES order by SALARY desc)
+                where rownum < 8); --> the 7th max salary
+
 -- 11. Find the 10th maximum salary from the employees table (do not include duplicates)
+select SALARY from EMPLOYEES
+where SALARY = (select min(salary) from (select distinct salary from employees order by salary desc)
+              where rownum < 11);
+
 -- 12. Find the 3rd minimum salary from the employees table (do not include duplicates)
+select distinct salary from employees order by salary; --> all salary in asc order
+
+select SALARY from (select distinct salary from employees order by salary)
+where rownum < 4; --> first 3 salary ordered in asc order
+
+--solution
+select SALARY from EMPLOYEES
+where SALARY = (select max(SALARY) from (select distinct salary from employees order by salary)
+                where rownum < 4);
+
+
 -- 13. Find the 5th minimum salary from the employees table (do not include duplicates)
+select SALARY from EMPLOYEES
+where SALARY = (select max(SALARY) from (select distinct salary from employees order by salary)
+                where rownum < 6);
